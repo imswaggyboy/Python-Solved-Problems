bffe00f05e0ba20ad6ca6a7ae9a6dd19ef2eceef
Surya Murugan <75663923+imswaggyboy@users.noreply.github.com>
2023-04-11 15:17:27 +0530

    Create count_Odd_Numbers_in_an_Interval_Range.py

---
+++ leet/count_Odd_Numbers_in_an_Interval_Range.py
@@ -0,0 +1,30 @@
+---------------------------------------------------------------------
+brute force approach
+time:O(n)
+----------------------------------------------------------------------
+
+class Solution(object):
+    def countOdds(self, low, high):
+        lst = []
+        for num in range(low,high+1):
+            if num%2!=0:
+                lst.append(num)
+        return len(lst)
+
+
+---------------------------------------------------------------------
+jst a mathematics approach (guess what i dont i learnt from g4g) 
+time:O(1)
+----------------------------------------------------------------------
+
+class Solution(object):
+    def countOdds(self, low, high):
+        n = high-low+1
+        if n%2==0:
+            return n//2
+        else:
+            if high%2:
+                return n//2+1
+            else:
+                return n//2
+              
